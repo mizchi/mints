0b5c02101fa461cd6968dcb3f65128238d7f6b55
mizchi <miz404@gmail.com>
2021-10-20 05:01:08 +0900

    Update

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 export default defineConfig({
   define: {
-    // "require.main": JSON.stringify(false),
-    "require.main === module": JSON.stringify(false),
-    // module: JSON.stringify(false),
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
   build: {
