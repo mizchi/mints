2244474b009944bba9ef4162b1d477ea59dcb03b
mizchi <miz404@gmail.com>
2021-11-29 16:24:03 +0900

    test262

--- packages/mints/test/preprocess.js
+++ packages/mints/test/preprocess.js
@@ -1,4 +1,5 @@
 const mints = require("../dist/index.cjs");
+const prettier = require("prettier");
 
 /**
  * test262-harness preprocessor documented here:
@@ -17,6 +18,9 @@ module.exports = function (test) {
   if (test.scenario === "default") {
     return null;
   }
+  // if (test.scenario === "strict mode") {
+  //   return null;
+  // }
 
   // TCO tests seem to fail in V8 normally, so skip those.
   if (test.attrs.features?.includes("tail-call-optimization")) {
@@ -24,16 +28,32 @@ module.exports = function (test) {
   }
 
   try {
+    // console.log("test", test);
     // console.log("transformed", test.file, test.contents.slice(0, 10));
-    console.log("test.contents", test.contents);
-    const transformed = mints.transformSync(test.contents);
+    // console.log("test.contents", test.contents);
+    // test.contents = prettier.format(test.contests, {
+    //   // filepath: "$.tsx",
+    //   parser: "typescript",
+    // });
+    // console.log("pre", prettiered);
+    const transformed = mints.transformSync(
+      prettier.format(test.contents, {
+        filepath: "$.tsx",
+        parser: "typescript",
+      })
+    );
     if (transformed.error) {
-      console.log("====", test.contents, "\n====\n");
+      // console.log("====", test.contents, "\n====\n");
       throw new Error("parse-error");
     }
     test.contents = transformed.code;
+
+    // if (test.scenario === "strict mode") {
+    //   test.contents = "'use strict';'\n" + test.contents;
+    // }
   } catch (error) {
-    // console.log("failed", test.file);
+    // console.log("failed", error);
+    // throw
     test.result = {
       stderr: JSON.stringify(error, null, 2),
       stderr: "error",
